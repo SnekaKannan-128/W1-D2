ed95c33ac7cefd98e508b7acdcfe4f0d7732cc0c
SnekaKannan-128 <149248992+SnekaKannan-128@users.noreply.github.com>
2024-03-07 19:33:16 +0530

    W1-D2

---
+++ FireFox.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo ='f';
+		
+		System.out.println("Browser Version : " + browserVersion);
+		System.out.println("Browser Name: " + browserName);
+		System.out.println("Is The Browser Visible: " + isVisible);
+		System.out.println("Released Year: " + releaseYear);
+		System.out.println("Browser Logo: "+ browserLogo);
+		
+				
+	}
+
+}
